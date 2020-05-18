019d596f4d996d57aa5f9e2e8f266742c1f774d1
xboyshark <48173833+xboyshark@users.noreply.github.com>
2020-05-19 00:07:22 +0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using Donor.Models;
+using WebGrease.Css.Ast;
+
+namespace Donor.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        DataClasses1DataContext dc = new DataClasses1DataContext();
+        public ActionResult Index()
+        {
+            ViewBag.Message = "View Page";
+            var selec = dc.donordatas.ToList();
+            return View(selec);
+            
+        }
+
+        public JsonResult GetSearchingData(string SearchBy,string SearchValue)
+        {
+            List<donordata> DList = new List<donordata>();
+             DList = dc.donordatas.Where(x => x.name.Contains(SearchValue) || SearchValue == null).ToList();
+                return Json(DList, JsonRequestBehavior.AllowGet);
+           
+
+        }
+        //public ActionResult category()
+        //{
+        //    //string b = Request["cat"];
+        //    //var selec = dc.donordatas.Where(c=> c.bloodtype.Equals(b));
+        //    return View(Request["cat"].ToString());
+
+        //}
+
+        public ActionResult Add()
+        {
+            ViewBag.Message = "Add Page";
+
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            dc.SubmitChanges();
+            ViewBag.Message = "Edit Page";
+            var selec = dc.donordatas.ToList();
+            return View(selec);
+            
+        }
+
+        public ActionResult EditV(int id)
+        {
+            Session["check"] = id;
+            var selec = dc.donordatas.First(c => c.Id==id);
+            dc.SubmitChanges();
+            return View(selec);
+
+        }
+        public ActionResult EditConfirm(int id)
+        {
+            if(Session["check"].Equals(id))
+            {
+                var selec = dc.donordatas.First(c => c.Id == id);
+                string name = Request["name"];
+                string bloodtype = Request["blood"];
+                int phNo = Int32.Parse(Request["phoneNo"]);
+
+                selec.name = name;
+                selec.bloodtype = bloodtype;
+                selec.phNo = phNo;
+                dc.SubmitChanges();
+            }
+            return RedirectToAction("Edit");
+        }
+
+        public ActionResult Addd()
+        {
+            string name = Request["name"];
+            string bloodtype = Request["blood"];
+            int phNo = Int32.Parse(Request["phoneNo"]);
+            donordata d = new donordata();
+            d.name = name;
+            d.bloodtype = bloodtype;
+            d.phNo = phNo;
+            dc.donordatas.InsertOnSubmit(d);
+            dc.SubmitChanges();
+            
+          return  RedirectToAction("Index");
+        }
+
+        
+
+       
+       
+
+        public ActionResult editt()
+        { int id = Int16.Parse(Request["ID"]);
+            
+            String newName = Request["NewName"];
+            String bt = Request["BT"];
+            int ph = Int32.Parse(Request["PH"]);
+            var selec = dc.donordatas.First(c=> c.Equals(id));
+            selec.name = newName;
+            selec.phNo = ph;
+            selec.bloodtype = bt;
+            dc.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/donor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Donor.Models
+{
+    public class donor
+    {
+        public int ID { set; get; }
+        public String Name { set; get; }
+        public String BloodType { set; get; }
+        public int phNo { set; get; }
+    }
+}
